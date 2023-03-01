3714206e5ef187c47dca2b6ee4d27bc37e7157e1
Annetta777 <loginaanna2000a@gmail.com>
2023-03-01 16:35:37 +0600

    add function drawBorder

--- snake.js
+++ snake.js
@@ -2,3 +2,14 @@ let canvas = document.getElementById ("canvas")
 let ctx = canvas.getContext ("2d")
 let width = canvas.width
 let height = canvas.height
+let blockSize = 10
+let widthInBlocks = width / blockSize
+let heightInBlocks = height / blockSize
+let score = 0 // score++ for eat an apple
+function drawBorder() {
+  ctx.fillStyle = "Gray"
+  ctx.fillRect(0, 0, width, blockSize)
+  ctx.fillRect(0, height - blockSize, width, blockSize)
+  ctx.fillRect(0, 0, blockSize, height)
+  ctx.fillRect(width - blockSize, 0, blockSize, height)
+  }
